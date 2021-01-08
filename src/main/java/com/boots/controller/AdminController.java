a41f0bd9704c9eddac19cf98985b1f46316e8fc1
fedorGOD <59053843+fedorGODUS@users.noreply.github.com>
2021-01-08 12:33:45 +0300

    Last commit. Especially for Sanya.

--- src/main/java/com/boots/controller/AdminController.java
+++ src/main/java/com/boots/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+//no hibernate config
 @Controller
 public class AdminController {
     @Autowired
